3cf8c773abf5e5ad775445698dec32f4cec49350
Giotje <devgio@icloud.com>
2021-05-01 19:04:36 +0200

    added signOut button for testing purposes

--- Quaerere_client/src/screens/HomeScreen/HomeScreen.jsx
+++ Quaerere_client/src/screens/HomeScreen/HomeScreen.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { Text, View } from "react-native";
-
+import styles from './styles'
+import firebase from '../../firebase/config.js'
 export default function HomeScreen(props) {
   const user = props.user;
-  console.log(user);
+
+  const signOut = () =>{
+    firebase.auth().signOut().then(() => alert("successfully signed out!"))
+  }
   return (
-    <View>
+    <View style={styles.container}>
       <Text>username = {user.name}</Text>
       <Text>user email = {user.email} </Text>
+    <View style={styles.footerView}>
+      <Text style={styles.footerLink} onPress={() => signOut()}> Sign out?</Text>
+    </View>
     </View>
   );
 }
---
+++ Quaerere_client/src/screens/HomeScreen/styles.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  footerView: {
+    flex: 1,
+    alignItems: "center",
+    marginTop: 20,
+  },
+  footerText: {
+    fontSize: 16,
+    color: "#2e2e2d",
+  },
+  footerLink: {
+    color: "#788eec",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+});
